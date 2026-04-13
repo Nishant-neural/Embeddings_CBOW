db045c1f470b8b5c50e759a7ebb24c5a36d6d8ef
Nishant-neural <nishantiiwari@gmail.com>
2026-04-13 19:57:08 +0530

    Created a minimal cbow datatset in pytorch

---
+++ embeddings.py
@@ -0,0 +1,39 @@
+import re
+from collections import Counter
+import torch
+
+text = """
+the movie was great and the acting was amazing
+the film was terrible and boring
+i love this movie
+i hate this movie
+"""
+
+text = text.lower()
+text = re.sub(r"[^\w\s]", "", text)
+
+tokens = text.split()
+
+vocab = Counter(tokens)
+word2idx = {word: i for i, word in enumerate(vocab)}
+idx2word = {i: word for word, i in word2idx.items()}
+
+vocab_size = len(word2idx)
+print(vocab_size)
+
+window_size = 2  
+data = []
+for i in range(window_size, len(tokens) - window_size):
+    context = []
+    
+    for j in range(-window_size, window_size + 1):
+        if j != 0:
+            context.append(word2idx[tokens[i + j]])
+    
+    target = word2idx[tokens[i]]
+    
+    data.append((context, target))
+  
+
+contexts = torch.tensor([x[0] for x in data])
+targets = torch.tensor([x[1] for x in data])
