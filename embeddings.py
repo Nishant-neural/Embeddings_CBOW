4e810d3869499a00c3d24cc9f1c591613deca953
Nishant-neural <nishantiiwari@gmail.com>
2026-04-16 15:37:57 +0530

    added modl using pretrained embeddings

--- embeddings.py
+++ embeddings.py
@@ -86,3 +86,18 @@ def find_similar(word, top_k=3):
     return similarities[1:top_k+1]
 
 print(find_similar("movie"))
+
+class SentimentModel(nn.Module):
+    def __init__(self, embedding_matrix):
+        super().__init__()
+        
+        vocab_size, embed_dim = embedding_matrix.shape
+        
+        self.embedding = nn.Embedding.from_pretrained(embedding_matrix)
+        self.fc = nn.Linear(embed_dim, 1)
+    
+    def forward(self, x):
+        x = self.embedding(x)
+        x = x.mean(dim=1)
+        x = self.fc(x)
+        return x
