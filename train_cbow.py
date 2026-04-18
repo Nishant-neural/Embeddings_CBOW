1c2643f5d96f5fc951008848b624b84c74a33ca0
Nishant-neural <nishantiiwari@gmail.com>
2026-04-18 23:45:55 +0530

    now  use IMDB dataset for text preparation and cbow training

--- train_cbow.py
+++ train_cbow.py
@@ -1,17 +1,18 @@
 import torch
 import torch.nn as nn
+from torchtext.datasets import IMDB
 from data import prepare_data
 from cbow_model import CBOW
 
 
-text = """
-the movie was great and the acting was amazing
-the film was terrible and boring
-i love this movie
-i hate this movie
-"""
+train_iter, test_iter = IMDB(split=('train', 'test'))
+all_text = ""
+for label, text in train_iter:
+    all_text += text + " "
+for label, text in test_iter:
+    all_text += text + " "
 
-contexts, targets, vocab, word2idx, idx2word, vocab_size = prepare_data(text)
+contexts, targets, vocab, word2idx, idx2word, vocab_size = prepare_data(all_text)
 
 model = CBOW(vocab_size, embed_dim=50)
 
